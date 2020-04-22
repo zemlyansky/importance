a40ce3cca5bc0d84860f043057e65b4cb1fc1149
zemlyansky <anton.zemlyansky@gmail.com>
2020-04-22 19:59:42 +0300

    .

---
+++ index.js
@@ -0,0 +1,46 @@
+var score = require('./score')
+
+function permutationScores (model, X, y, kind, id, nRepeats) {
+  const scores = []
+  for (let r = 0; r < nRepeats; r++) {
+    const Xclone = JSON.parse(JSON.stringify(X))
+    for (let i = Xclone.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[Xclone[i][id], Xclone[j][id]] = [Xclone[j][id], Xclone[i][id]]
+    }
+    scores.push(score(model, Xclone, y, kind))
+  }
+  return scores
+}
+
+module.exports = function importance (model, X, y, opts = {}) {
+  const log = opts.verbose ? console.log : () => {}
+  const kind = opts.kind ? opts.kind : (Array.from(new Set(y)).length > 10) ? 'mae' : 'ce'
+  const nRepeats = opts.n || 1
+  const onlyMeans = opts.means || (nRepeats === 1)
+  const baseScore = score(model, X, y, kind)
+  const nFeatures = X[0].length
+  log('Start feature importance')
+  log('Score: %s, N repeats: %d, N features: %d', kind, nRepeats, nFeatures)
+  log('Base score:', baseScore)
+
+  const importances = []
+  for (let i = 0; i < nFeatures; i++) {
+    log('Computing importance of feature:', i)
+    importances.push(permutationScores(model, X, y, kind, i, nRepeats).map(imp => baseScore - imp))
+  }
+
+  // if (opts.scale) { }
+
+  const importancesMeans = importances.map(imps => imps.reduce((a, v) => a + v / nRepeats))
+  const importancesStds = importances.map((imps, i) => {
+    const std = Math.sqrt(imps.reduce((a, v) => a + Math.pow(v - importancesMeans[i], 2) / nRepeats, 0))
+    return std
+  })
+
+  return onlyMeans ? importancesMeans : {
+    importances,
+    importancesMeans,
+    importancesStds
+  }
+}
---
+++ score.js
@@ -0,0 +1,58 @@
+function mae (yt, yp) {
+  return yt.reduce((a, v, i) => a + Math.abs(v - yp[i]) / yt.length, 0)
+}
+
+function mse (yt, yp) {
+  return yt.reduce((a, v, i) => a + Math.pow(v - yp[i], 2) / yt.length, 0)
+}
+
+function rmse (yt, yp) {
+  return Math.sqrt(mse(yt, yp))
+}
+
+function smape (yt, yp) {
+  const sum = yt.reduce((a, v, i) => a + Math.abs(v - yp[i]) / (Math.abs(v) + Math.abs(yp[i])), 0)
+  return (sum / yt.length) * 100
+}
+
+function acc (yt, yp) {
+  return yt.reduce((a, v, i) => a + (v === yp[i]) / yt.length, 0)
+}
+
+function ce (yt, yp) {
+  const eps = 1e-10
+  const ce = []
+  for (let i = 0; i < yt.length; i++) {
+    // Limit probs to (eps, 1 - eps)
+    const probs = yp[i].map(classProb => classProb < eps ? eps : classProb > (1 - eps) ? 1 - eps : classProb)
+    const sum = probs.reduce((p, v) => p + v)
+    ce.push(
+      -probs
+        .map(p => p / sum)
+        .map((p, i) => Math.log(p) * (yt[i] === i))
+        .reduce((a, v) => a + v)
+    )
+  }
+  return ce.reduce((a, v) => a + v / yt.length)
+}
+
+const scoreTypes = {
+  mae, mse, rmse, smape, acc, ce
+}
+
+// Score a model given input X and target y
+function score (model, X, y, kind) {
+  if (y.length !== X.length) {
+    throw new Error('Arrays have different length')
+  } else if (!y.length) {
+    throw new Error('Zero length array')
+  } else if (kind === 'ce') {
+    return scoreTypes[kind](y, model.predictProba(X))
+  } else if (kind === 'acc') {
+    return scoreTypes[kind](y, model.predict(X))
+  } else {
+    return -scoreTypes[kind](y, model.predict(X))
+  }
+}
+
+module.exports = score
